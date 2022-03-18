a18436dcc700720753f6f3e36dd690a8b8583cad
harveyywang <harveyywang@tencent.com>
2022-03-18 20:08:58 +0800

    add: 学习指针 结构 方法 Embvedding

---
+++ 15-closures/closures.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+
+// func() 后面的int是干嘛的
+func intSeq() func() int {
+	i := 0
+	return func() int {
+		i++
+		return i
+	}
+}
+
+func main() {
+	nextInt := intSeq()
+
+	fmt.Println(nextInt())
+	fmt.Println(nextInt())
+	fmt.Println(nextInt())
+
+	newInt := intSeq()
+	fmt.Println(newInt())
+
+}
